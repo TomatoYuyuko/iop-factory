1a23ba9ffe737c252755f40dd6cb0dd1ab724419
Mercury <dxmo_001@qq.com>
2019-02-14 18:39:50 +0800

    dev update

--- iop-project/src/views/Index.vue
+++ iop-project/src/views/Index.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="container">
-        <el-tabs type="border-card">
-            <el-tab-pane label="装备普造">
-                <equip></equip>
-            </el-tab-pane>
-            <el-tab-pane label="装备重造">
-                <fairy></fairy>
-            </el-tab-pane>
-        </el-tabs>
+        <el-row>
+            <el-col :span="16" :offset="4">
+                <el-tabs type="border-card">
+                    <el-tab-pane label="装备普造">
+                        <equip></equip>
+                    </el-tab-pane>
+                    <el-tab-pane label="装备重造">
+                        <fairy></fairy>
+                    </el-tab-pane>
+                </el-tabs>
+
+            </el-col>
+        </el-row>
 
     </div>
 </template>
@@ -26,8 +31,7 @@
 </script>
 
 <style scoped>
-    .container {
-        position: relative;
-        margin: 20px 500px;
-    }
+.container{
+    margin-top: 20px;
+}
 </style>
